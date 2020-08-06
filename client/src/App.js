e133d0b4bb7a341747a71d5990d581cb531f6a89
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-06 09:05:55 +0530

    Seems to be broken if running with StrictMode enabled.

--- client/src/App.js
+++ client/src/App.js
@@ -8,24 +8,43 @@ import {
 import './css/main.css';
 
 function App() {
+  const ref = useRef([])
+  const [items, set] = useState([])
+  const transitions = useTransition(items, null, {
+    from: 
+      { opacity: 0, height: 0, innerHeight: 0, transform: 'perspective(600px) rotateX(0deg)', color: '#8fa5b6' },
+    enter: [
+      { opacity: 1, height: 80, innerHeight: 80 },
+      { transform: 'perspective(600px) rotateX(180deg)', color: '#28d79f' },
+      { transform: 'perspective(600px) rotateX(0deg)' },
+    ],
+    leave: [{ color: '#c23369' }, { innerHeight: 0 }, { opacity: 0, height: 0 }],
+    update: { color: '#28b4d7' },
+  })
+
+  const reset = useCallback(() => {
+    ref.current.map(clearTimeout)
+    ref.current = []
+    set([])
+    ref.current.push(setTimeout(() => set(['Shopping', 'Slots', '.com']), 2000))
+    ref.current.push(setTimeout(() => set(['Your', 'Time']), 4000))
+    ref.current.push(setTimeout(() => set(['Your', 'Store']), 6000))
+    ref.current.push(setTimeout(() => set(['Book', 'Now']), 8000))
+  }, [])
+
+  useEffect(() => { 
+    reset(); }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {transitions.map(({ item, props: { innerHeight, ...rest }, key }) => (
+        <animated.div className="transitions-item" key={key} style={rest} onClick={reset}>
+          <animated.div style={{ overflow: 'visible', height: innerHeight }}>{item}</animated.div>
+        </animated.div>
+      ))}
     </div>
-  );
+  )
 }
+
 
 export default App;
